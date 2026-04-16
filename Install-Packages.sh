6363661fa98e233ae4ac741afcdf87ff09590239
Gustavo Merkel B. <gusthvv@gmail.com>
2026-04-15 22:10:34 -0300

    Update Install-Packages.sh

--- Install-Packages.sh
+++ Install-Packages.sh
@@ -18,9 +18,9 @@ yay -S --needed --noconfirm \
 git curl flatpak unrar unzip 7zip code obs-studio firefox chromium \
 rofi hyprpaper waybar mako fastfetch cava nerd-fonts pavucontrol \
 btop nvim steam pcmanfm jre-openjdk discord wine wine-gecko \
-wine-mono winetricks wl-clipboard cliphist grim slurp spotify \
+wine-mono winetricks wl-clipboard cliphist spotify \
 lxappearance grim slurp rocm-smi-lib jq gvfs gvfs-mtp gvfs-smb \
-gvfs-nfsgvfs-afc gvfs-goa gvfs-gphoto2 udisks2 udiskie ntfs-3g \
+gvfs-nfs gvfs-afc gvfs-goa gvfs-gphoto2 udisks2 udiskie ntfs-3g \
 dosfstools exfatprogs polkit lxsession tumbler ffmpegthumbnailer \
 jmtpfs libmtp
 
